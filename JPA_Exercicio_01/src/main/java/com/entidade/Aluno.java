3076400b94589dd8226f3fc891871b0855f0852c
Alécio <alecio.godoi@ymail.com>
2021-08-27 08:41:37 -0300

    Atualização

---
+++ JPA_Exercicio_01/src/main/java/com/entidade/Aluno.java
@@ -0,0 +1,67 @@
+package com.entidade;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Aluno {
+	// Atributos
+	@Id					// Define 'ra' como chave primária
+	private String ra;
+	
+	private String nome;
+	private String email;
+	private double nota1;
+	private double nota2;
+	
+	// Construtores
+	public Aluno() {
+	}
+	public Aluno(String ra, String nome, String email, double nota1, double nota2) {
+		super();
+		this.ra = ra;
+		this.nome = nome;
+		this.email = email;
+		this.nota1 = nota1;
+		this.nota2 = nota2;
+	}
+	
+	// Getters e Setters
+	public String getRa() {
+		return ra;
+	}
+	public void setRa(String ra) {
+		this.ra = ra;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public double getNota1() {
+		return nota1;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public double getNota2() {
+		return nota2;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	
+	@Override
+	public String toString() {
+		return "Aluno [ra=" + ra + ", nome=" + nome + ", email=" + email + ", nota1=" + nota1 + ", nota2=" + nota2
+				+ "]";
+	}
+}
---
+++ JPA_Exercicio_01/src/main/java/com/servicos/AlunoCRUD.java
@@ -0,0 +1,36 @@
+package com.servicos;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.entidade.Aluno;
+
+public class AlunoCRUD {
+	
+	public void insert(Aluno aluno) {
+		// Unidade de Persistencia
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("JPA_01");
+		// Criar objeto para fazer o gerenciamento das classes com as tabelas
+		EntityManager entitymanager = emfactory.createEntityManager();
+		
+		entitymanager.getTransaction().begin();   // abre a transação
+		entitymanager.persist(aluno);             // inserir aluno
+		entitymanager.getTransaction().commit();  // fecha a transação
+		entitymanager.close();                    // fecha o gerenciador de entidades
+		emfactory.close();                        // fecha a unidade de persistencia
+	}
+	
+	public void update(Aluno aluno) {
+		
+	}
+
+	public void delete(String ra) {
+		
+	}
+
+	public Aluno find(String ra) {
+		return null;
+	}
+	
+}
