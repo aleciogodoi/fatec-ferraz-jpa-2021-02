da328d605c13c3f25954d4c52975fa2479915faa
Alécio <alecio.godoi@ymail.com>
2021-09-03 10:35:47 -0300

    JPA_04

---
+++ JPA_04/src/main/java/com/eclipselink/entity/Cliente.java
@@ -0,0 +1,82 @@
+package com.eclipselink.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance( strategy = InheritanceType.TABLE_PER_CLASS )
+
+public abstract class Cliente implements Serializable   {
+	
+	@Id						// Chave Primária Cliente
+	private int idCliente;
+	
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private String email;
+	
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(int idCliente, String nome, String endereco, String telefone, String email) {
+		super();
+		this.idCliente = idCliente;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.email = email;
+	}
+
+	public int getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [idCliente=" + idCliente + ", nome=" + nome + ", endereco=" + endereco + ", telefone="
+				+ telefone + ", email=" + email + "]";
+	}
+
+}
