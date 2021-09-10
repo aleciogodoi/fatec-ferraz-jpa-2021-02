a104b7af4577f494e09dd29b222d3160bd843f3f
Alécio <alecio.godoi@ymail.com>
2021-09-10 09:07:55 -0300

    JPA 07

---
+++ JPA_07/src/main/java/com/eclipselink/entity/Departamento.java
@@ -0,0 +1,43 @@
+package com.eclipselink.entity;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Departamento {
+	@Id
+	private int idDepto;
+	private String nomeDepto;
+	
+	@OneToMany( targetEntity = Funcionario.class )
+	private List funcionario;
+
+	
+	public Departamento() {
+	}
+	public Departamento(int idDepto, String nomeDepto, List funcionario) {
+		this.idDepto = idDepto;
+		this.nomeDepto = nomeDepto;
+		this.funcionario = funcionario;
+	}
+	public int getIdDepto() {
+		return idDepto;
+	}
+	public void setIdDepto(int idDepto) {
+		this.idDepto = idDepto;
+	}
+	public String getNomeDepto() {
+		return nomeDepto;
+	}
+	public void setNomeDepto(String nomeDepto) {
+		this.nomeDepto = nomeDepto;
+	}
+	@Override
+	public String toString() {
+		return "Departamento [idDepto=" + idDepto + ", nomeDepto=" + nomeDepto + 
+				", funcionario=" + funcionario + "]";
+	}
+	
+}
