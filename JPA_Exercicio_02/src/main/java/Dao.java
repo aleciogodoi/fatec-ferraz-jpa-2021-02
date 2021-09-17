9cded5214ce344957dc5e922780d1e13a21bf208
Alécio <alecio.godoi@ymail.com>
2021-09-17 09:41:38 -0300

    Exercicio 02

--- JPA_Exercicio_02/src/main/java/Dao.java
+++ JPA_Exercicio_02/src/main/java/Dao.java
@@ -1,5 +1,132 @@
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 public class Dao {
 	
+	public static void insert(Pessoa pessoa) {
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("JPA_EX02");
+		EntityManager entitymanager = emfactory.createEntityManager();
+		
+		entitymanager.getTransaction().begin();
+		entitymanager.persist(pessoa);
+		entitymanager.getTransaction().commit();
+		
+		emfactory.close();
+		entitymanager.close();
+	}
 	
+	public static void update(Pessoa pessoa) {
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("JPA_EX02");
+		EntityManager entitymanager = emfactory.createEntityManager();
+		
+		entitymanager.getTransaction().begin();
+		entitymanager.merge(pessoa);
+		entitymanager.getTransaction().commit();
+		
+		emfactory.close();
+		entitymanager.close();		
+	}
+	
+	public static void delete(int id) {
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("JPA_EX02");
+		EntityManager entitymanager = emfactory.createEntityManager();
+		
+		entitymanager.getTransaction().begin();
+		Query query = entitymanager.createNamedQuery("delete Pessoa");
+		query.setParameter("id", id);
+		query.executeUpdate();
+		entitymanager.getTransaction().commit();
+		
+		emfactory.close();
+		entitymanager.close();		
+	}
+
+	public static Pessoa findId(int id) {
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("JPA_EX02");
+		EntityManager entitymanager = emfactory.createEntityManager();
+		Pessoa pessoa = new Pessoa();
+		try {
+			Query query = entitymanager.createNamedQuery("find Pessoa id");
+			query.setParameter("id", id);
+			pessoa = (Pessoa) query.getSingleResult();
+		} catch (Exception e) {
+			pessoa = null;
+		} finally {
+			emfactory.close();
+			entitymanager.close();
+			return pessoa;
+		}
+	}
+
+	public static Pessoa findCPF(String cpf) {
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("JPA_EX02");
+		EntityManager entitymanager = emfactory.createEntityManager();
+		Pessoa pessoa = new Pessoa();
+		try {
+			Query query = entitymanager.createNamedQuery("find Pessoa CPF");
+			query.setParameter("cpf", cpf);
+			pessoa = (Pessoa) query.getSingleResult();
+		} catch (Exception e) {
+			pessoa = null;
+		} finally {
+			emfactory.close();
+			entitymanager.close();
+			return pessoa;
+		}
+	}
+
+	public static Pessoa findRG(String rg) {
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("JPA_EX02");
+		EntityManager entitymanager = emfactory.createEntityManager();
+		Pessoa pessoa = new Pessoa();
+		try {
+			Query query = entitymanager.createNamedQuery("find Pessoa RG");
+			query.setParameter("rg", rg);
+			pessoa = (Pessoa) query.getSingleResult();
+		} catch (Exception e) {
+			pessoa = null;
+		} finally {
+			emfactory.close();
+			entitymanager.close();
+			return pessoa;
+		}
+	}
+
+	public static List<Pessoa> findNome(String nome) {
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("JPA_EX02");
+		EntityManager entitymanager = emfactory.createEntityManager();
+		List<Pessoa> pessoas = null;
+		try {
+			Query query = entitymanager.createNamedQuery("find Pessoa Nome");
+			query.setParameter("nome", nome);
+			pessoas = (List<Pessoa>) query.getResultList();
+		} catch (Exception e) {
+			pessoas = null;
+		} finally {
+			emfactory.close();
+			entitymanager.close();
+			return pessoas;
+		}
+	}
+
+	public static List<Pessoa> all() {
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("JPA_EX02");
+		EntityManager entitymanager = emfactory.createEntityManager();
+		List<Pessoa> pessoas = null;
+		try {
+			Query query = entitymanager.createNamedQuery("list Pessoa");
+			pessoas = (List<Pessoa>) query.getResultList();
+		} catch (Exception e) {
+			pessoas = null;
+		} finally {
+			emfactory.close();
+			entitymanager.close();
+			return pessoas;
+		}
+	}
+
 }
--- JPA_Exercicio_02/src/main/java/Pessoa.java
+++ JPA_Exercicio_02/src/main/java/Pessoa.java
@@ -1,28 +1,52 @@
 import java.sql.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 @Entity
+@NamedQueries ({
+	@NamedQuery(name="find Pessoa id", query ="Select p From Pessoa p Where p.idPessoa = :id"),
+	@NamedQuery(name="find Pessoa CPF", query ="Select p From Pessoa p Where p.cpf = :cpf"),
+	@NamedQuery(name="find Pessoa RG", query ="Select p From Pessoa p Where p.rg = :rg"),
+	@NamedQuery(name="find Pessoa Nome", query ="Select p From Pessoa p Where p.nome like :nome"),
+	@NamedQuery(name="list Pessoa", query ="Select p From Pessoa p"),
+	@NamedQuery(name="delete Pessoa", query ="Delete From Pessoa Where p.idPessoa = :id")
+})
 public class Pessoa {
 	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto incremento
 	@Id
+	@Column(name = "idPessoa")
 	private int idPessoa;
 	
+	@Column(name ="Nome", columnDefinition="VARCHAR(100)", nullable = false)
 	private String nome;
+
+	@Column(name ="CPF", columnDefinition="VARCHAR(15)", nullable = false)
 	private String cpf;
+
+	@Column(name ="RG", columnDefinition="VARCHAR(12)", nullable = false)
 	private String rg;
+
+	@Column(name ="dtNascimento", columnDefinition="Date", nullable = false)
 	private Date dtNascimento;
+	
+	@Column(name ="Altura", columnDefinition="DECIMAL(5,2)", nullable = false)
 	private float altura;
+
+	@Column(name ="Peso", columnDefinition="DECIMAL(5,2)", nullable = false)
 	private float peso;
 
 	public Pessoa() {
 	}
 
-	public Pessoa(int idPessoa, String nome, String cpf, String rg, 
+	public Pessoa(String nome, String cpf, String rg, 
 					Date dtNascimento, float altura, float peso) {
-		super();
-		this.idPessoa = idPessoa;
 		this.nome = nome;
 		this.cpf = cpf;
 		this.rg = rg;
@@ -92,6 +116,5 @@ public class Pessoa {
 		return "Pessoa [idPessoa=" + idPessoa + ", nome=" + nome + ", cpf=" + cpf + ", rg=" + rg + ", dtNascimento="
 				+ dtNascimento + ", altura=" + altura + ", peso=" + peso + "]";
 	}
-	
-	
+
 }
