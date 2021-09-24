8d8b051164a78282da0aeec828d33dc2f54c6495
Alécio <alecio.godoi@ymail.com>
2021-09-24 10:54:57 -0300

    Exercicio NF

--- JPA_Exercicio_03_NF/src/main/java/com/entidades/Nota.java
+++ JPA_Exercicio_03_NF/src/main/java/com/entidades/Nota.java
@@ -1,6 +1,11 @@
 package com.entidades;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 public class Nota {
 	private int nr;
@@ -9,11 +14,19 @@ public class Nota {
 	
 	public Nota() {
 	}
-	public Nota(int nr, Date data, float total) {
-		super();
+	
+	@OneToMany
+	private List<Item> itens = new ArrayList<>();
+	
+	@ManyToOne
+	private Cliente cliente;
+	
+	public Nota(int nr, Date data, List<Item> itens, Cliente cliente) {
 		this.nr = nr;
 		this.data = data;
-		this.total = total;
+		this.itens = itens;
+		this.cliente = cliente;
+		this.setTotal();
 	}
 	public int getNr() {
 		return nr;
@@ -30,12 +43,28 @@ public class Nota {
 	public float getTotal() {
 		return total;
 	}
-	public void setTotal(float total) {
-		this.total = total;
+	public void setTotal() {
+		this.total=0f;
+		for(Item item: itens)
+			this.total += item.getTotalItem();
+	}
+	public List<Item> getItens() {
+		return itens;
+	}
+	public void setItens(List<Item> itens) {
+		this.itens = itens;
+		this.setTotal();
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
 	}
 	@Override
 	public String toString() {
-		return "Nota [nr=" + nr + ", data=" + data + ", total=" + total + "]";
+		return "Nota [nr=" + nr + ", data=" + data + ", total=" + total + ", itens=" + itens + ", cliente=" + cliente
+				+ "]";
 	}
 	
 }
